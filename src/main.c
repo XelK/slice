5ffb42e8f9f5306e7133e8e752d425c9d48d1da2
xelk <haveiteasy@gmail.com>
2021-05-11 22:43:30 +0200

    working solution

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
  #include <string.h>			// Per memset()
  #include <unistd.h>			// Per close()
  #include <stdlib.h>			// Per atoi() e exit()
-// #include <sys/time.h>		// Per struct timeval
+ #include <time.h>		
 
 #define PORT 8080
 #define MAXPENDING 5
@@ -24,8 +24,6 @@ int main(){
     int sock;
     int client;
     struct sockaddr_in addr;
-    int pid;
-
 
    if ((sock = socket(AF_INET,SOCK_STREAM,0))<0){
         fprintf(stderr,"Error in socket creation!\n");
@@ -59,20 +57,16 @@ int main(){
 
 }
 
-
 void handleClient(int client){
     char message[BUFFERSIZE];
     int msg;
 
-
     memset(&message,0,sizeof(message));
-
+    
     if((msg=recv(client,message,BUFFERSIZE,0))<0){
         fprintf(stderr,"Errore in ricezione!\n");
         exit(1);
     }
-
-    printf("Messaggio ricevuto:\n%s\n",message);
 
     char from[50]=" "; 
     char to[50];
@@ -93,21 +87,18 @@ void handleClient(int client){
             if (message[++i]==' ')
                 from[c]='\0';
         }
-        printf("from: %s\n",from);
 
-        if (strcmp(from," ")==0) // index.html
+        if (strcmp(from," ")==0) // index
             prepMessage(200,from," ",message);
         else if (strcmp(temp=search(from),"")!=0){ // found in DB
-            prepMessage(302,from,temp,message);
+            prepMessage(301,from,temp,message);
         }
         else
             prepMessage(404,from,"",message);
     }
     else if(strstr(message,"POST")){
-        printf("POST find!\n");
         temp=strstr(message,"url:");
-        printf("temp: %s\n",temp);
-
+    
         int x=(strlen("url:"));
         int z=0;
         while(x <= strlen(temp)){
@@ -123,8 +114,6 @@ void handleClient(int client){
             printf("DB %d: %s\n",i,DB[i]);
         }
         prepMessage(201,from,to,message);
-    
-
     }
     else{
         printf("ERRORE! Methodo non supportato!\n");
@@ -132,11 +121,9 @@ void handleClient(int client){
     }
 
     printf("Message to reply:\n%s\nfine messaggio!\n",message);
-    printDB(DB);
- //   if (send(client,message,sizeof(message),0)!=sizeof(message)){
     if (send(client,message,strlen(message),0)!=strlen(message)){
         printf("errore in send\n");
-    //    exit(1);
+        exit(1);
     }
 
 }
@@ -151,37 +138,46 @@ char * search(char from[]){
 }
 
 void prepMessage(int code, char from[], char to[], char * message){
-    printf("Responding...\n");
-    printf("- from: %s\n- to:%s\n",from,to);
+
+    char t_date[1000];                                                     
+    time_t now = time(0);
+    struct tm tm = *gmtime(&now);
+    strftime(t_date, sizeof(t_date), "%a, %d %b %Y %H:%M:%S %Z", &tm);
 
     if (code==200){
-        printf("PREP-200\n");
-        strcpy(message,"HTTP/1.1 200 OK\nDate: Mon, 23 May 2005 22:38:34 GMT\nContent-Type: text/html; charset=UTF-8\nConnection: close\r\n\r\n");
+        strcpy(message,"HTTP/1.1 200 OK\nDate: "); 
+        strcat(message,t_date);
+        strcat(message,"\nContent-Type: text/html; charset=UTF-8\nConnection: close\r\n\r\n");
         strcat(message,"<!DOCTYPE html>\n<html>\n<head>\n<meta charset=\"UTF-8\">\n<title> Hello from slice server!</title>\n</head>\n<body>\n<center><h1>Slice server!</h1></center>\n</body>\n</html>");
     }
     if (code==201){
-        printf("PREP-201\n");
         printf("recive: %s\n",from);
-        strcpy(message,"HTTP/1.1 200 OK\nDate: Mon, 23 May 2005 22:38:34 GMT\nContent-Type: text; charset=UTF-8\nConnection: close\r\n\r\n");
+        strcpy(message,"HTTP/1.1 200 OK\nDate: ");
+        strcat(message,t_date);
+        strcat(message,"\nContent-Type: text; charset=UTF-8\nConnection: close\r\n\r\n");
         strcat(message,from);
         strcat(message,"\n");
         strcat(message,to);
     }
-    if(code == 302){
-        printf("PREP-301\n");
-        strcpy(message,"HTTP/1.1 301 Moved Permanently\nServer: slice\nDate: Mon, 03 May 2021 17:44:08 GMT\nContent-Type: text/html; charset=UTF-8\n");
+    if(code == 301){
+        strcpy(message,"HTTP/1.1 301 Moved Permanently\nServer: slice\n");
+        strcat(message,t_date);
+        strcat(message,"\nContent-Type: text/html; charset=UTF-8\n");
         strcat(message,"Location: ");
         strcat(message,to);
         strcat(message,"\nAccept-Ranges: bytes\nConnection: close\r\n\r\n");
-        strcat(message,"<!DOCTYPE html>\n<html>\n<head>\n<title>301 Moved Permanently</title>\n</head>\n<body>\n<center><h1>301 Moved Permanently</h1></center>\n</body>\n</html>");
+        strcat(message,"<!DOCTYPE html>\n<html>\n<head>\n<title>301 Moved Permanently</title>\n</head>\n<body>\n<center><h1>301 Moved Permanently to: ");
+        strcat(message,to);
+        strcat(message,"</h1></center>\n</body>\n</html>");
     }
     if(code==404){
-        printf("PREP-404\n");
         strcpy(message,"HTTP/1.1 404\ncontent-type: text/html\r\n\r\n");
         strcat(message,"<!DOCTYPE html>\n<html>\n<head>\n<meta charset=\"UTF-8\">\n<title>404 Not found!</title>\n</head>\n<body>\n<center><h1>404 Page Not found!</h1></center>\n</body>\n</html>");
     }
-        printf("Message to send: %s\n\n",message);
-        printf("FINE!\n");
+    if(code==405){
+        strcpy(message,"HTTP/1.1 405\ncontent-type: text/html\r\n\r\n");
+        strcat(message,"<!DOCTYPE html>\n<html>\n<head>\n<meta charset=\"UTF-8\">\n<title>405 Not found!</title>\n</head>\n<body>\n<center><h1>404 Page Not found!</h1></center>\n</body>\n</html>");
+    }
 }
 
 int updateDB(char DB[MAXDB][MAXURL], char from[]){
@@ -197,10 +193,4 @@ int updateDB(char DB[MAXDB][MAXURL], char from[]){
             printf("cane!\n");
     }
     return -1;
-}
-
-void printDB(char DB[MAXDB][MAXURL]){
-    for(int i=0; i<MAXDB; i++){
-        printf("DB[%d]:%s\n",i,DB[i]);
-    }
 }
